d5e528a1ea27afabc696fbe2ef48d1c5efe150a7
Mallarie <mallariemilano@gmail.com>
2019-05-29 09:19:08 -0700

    added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.Header}>
       <nav>
         <Link to="/red">Red</Link>
         <Link to="/blue">Blue</Link>
